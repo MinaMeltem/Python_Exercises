b7033d2b7f434ab679431560f2d92e38d02cf21e
unknown <MinaMeltem>
2018-01-28 14:18:05 -0500

    Add format specification

--- assigment1.py
+++ assigment1.py
@@ -3,10 +3,10 @@
 # Asking user to enter the price
 meal = float(input("Enter the price: "))
 
-#Adding tax and tip, and calculate the total
+#Adding tax and tip, and calculating the total
 tax = meal * 0.18
 tip = meal * 0.20
 total = meal + tax + tip
 
 #Displaying the total amount
-print ( "Your total is : "+ str(total))
+print ( "Your total is :$%.2f" % total)
