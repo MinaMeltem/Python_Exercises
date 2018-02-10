90dd427d357df614752aca153e6792efbcd856da
unknown <MinaMeltem>
2018-02-10 18:50:45 -0500

    Grade Calculator_Avarega bug fixed

--- assigment2_gradeCalculator.py
+++ assigment2_gradeCalculator.py
@@ -8,10 +8,10 @@ Output should be Total and the avarge of the grades.
 (Use for loop , not while loop) 
 
 """
-
 # Loop through the list to get last item, if it isn't -1, 
 # promt user to enter new grade, and add it in to the list,
 # Terminate the loop if it's - 1
+
 grades = [0]
 total = 0
 
@@ -22,8 +22,10 @@ for g in grades:
         grade = input("Enter the grade: ")
         total = total + int(grade)
         grades.append(int(grade))
- 
-# In order the to terminate the loop,  last item in the list should be -1
-# but we cant use it to calculate total and avarge since it's not a grade.
-print ("Sum of the grades : " , total + 1)
-print ("Average of the grades : " , (total + 1) / len(grades))
+        
+total = total + 1    # removing -1 from the list since it's not a grade
+length =  len(grades) - 2
+
+print ("Sum of the grades : " , total)
+print ("Average of the grades : " , total / length )
+
