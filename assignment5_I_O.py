594f50f4675bcf8e37b8eff9af42735a7998a30d
unknown <MinaMeltem>
2018-02-18 12:10:37 -0500

    File I/O practice assignment

--- assignment5_I_O.py
+++ assignment5_I_O.py
@@ -16,8 +16,7 @@ for line in content:
     if ID not in tickets:
         tickets[ID] = price
     else:
-        tickets.update({ID:price})
-        
+        tickets.update({ID:price})        
     total += tickets[ID]
     lenght += 1
     
@@ -26,13 +25,12 @@ max_ID = max(tickets, key=tickets.get)
 min_ID = min(tickets, key=tickets.get)
 ave = total / lenght
 
-output = ('======================================================\n'
+print ('======================================================\n'
        '\t\t TICKET REPORT \t\t' 
        '======================================================\n'
        ' There are {} tickets in the database\n\n'
        ' Maximum Ticket price is ${}\n'
        ' Minimum Ticket price is ${}\n'
        ' Avarage Ticket price is ${:.2f}\n\n'
-       ' Thank you for using  ticket system!') .format(lenght, tickets[max_ID],tickets[min_ID], ave)
-
-print(output)
+       ' Thank you for using  ticket system!'.format(lenght, tickets[max_ID],tickets[min_ID], ave)
+    )
